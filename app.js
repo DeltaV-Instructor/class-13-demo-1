66ad4e2d33fbe08b422d1ce3a7a70c170e7e9c34
tektechnologies <info@tektechnologies.com>
2022-01-29 10:20:58 -0600

    add global lets

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-'use stict';
+'use strict';
+console.log('app js is connected');
+
+
+//Global variables
+let orderForm = document.getElementById('orderForm');
+let orders = document.getElementById('orders');
+console.log('is this what we expect?',{orderForm, orders});
+
+
+
+//Constructor function
+
+
+
+
+//functions 
+
+
+
+//add event listerner
+
